a157e7e4eeaa37a05a66b4a116b2bb2783a2e1af
Christy Llewellyn <christy.elizabeth@hotmail.co.uk>
2024-02-16 19:17:09 +0000

    Added basic function to create html file

--- index.js
+++ index.js
@@ -51,3 +51,33 @@ const promptUser = () => {
     ])
 };
 
+//function to write a README file
+const generateHTML = (answers) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+<body>
+  <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+  <div class="container-fluid py-5">
+    <h1 class="display-5 fw-bold">${answers.title}</h1>
+    <p class="col-md-8 fs-4">I am from ${answers.location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${answers.github}</li>
+      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+
+promptUser()
+  .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+  .then(() => console.log('Successfully wrote to index.html'))
+  .catch((err) => console.error(err));
+
